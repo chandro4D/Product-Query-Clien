4c4da27982b2b37a598f0d196cf71aa3fb691ffb
chandro4D <chandroshakhor7@gmail.com>
2024-05-15 21:22:23 +0600

    add recommendation card

--- src/Components/MyRecommendCard/MyRecommendCard.jsx
+++ src/Components/MyRecommendCard/MyRecommendCard.jsx
@@ -1,28 +1,40 @@
 
 
 const MyRecommendCard = ({ recommendOne }) => {
-    const { RecommendedproductName,recomenderName,RecommendedProductImage } = recommendOne;
+    const { RecommendedproductName, recomenderName, RecommendedProductImage,recommenderEmail,recommendDate } = recommendOne;
     return (
-        <div className="overflow-x-auto bg-slate-300 mb-2 w-[1200px] ml-[150px]">
-            <table className="table">  
-                <tbody>                                   
+        <div className="overflow-x-auto bg-slate-300 mb-2 w-[1300px] ml-[100px]">
+            <table className="table">
+                <tbody>
                     <tr>
                         <td>
-                            <div className="flex items-center gap-3">
-                               
-                                    <div className="mask mask-squircle w-[200px] h-[60px]">
-                                        <h3>RecommendedProductImage</h3>
-                                        <img src={RecommendedProductImage} />
-                                    </div>
-                                    <div className="ml-[300px] w-[200px] h-12">
-                                        <h2>{recomenderName}</h2>
-                                    </div>
-                               
+                            <div className=" flex rounded-sm">
+                                <div className="mr-12">
+                                    <h3 className="text-xl font-medium text-sky-600">Recommended Product Image</h3>
+
+                                    <img className="w-[150px] h-[100px] ml-[40px] rounded-xl" src={RecommendedProductImage} />
+                                </div>
+                                <div className="mr-12 ">
+                                    <h3 className="text-xl font-medium text-sky-600"> Recommended Product Name</h3>
+                                    <br />
+                                    <h3 className="text-2xl font-medium text-pink-600 text-center">{ RecommendedproductName}</h3>
+                                </div>
+                                <div className="mr-20 ">
+                                    <h3 className="text-xl font-medium text-sky-600"> Recomender Name</h3>
+                                    <br />
+                                    <h3 className="text-2xl font-medium text-pink-600 text-center">{ recomenderName}</h3>
+                                </div>
+                                <div className="text-center">
+                                    <h3 className="text-xl font-medium text-sky-600"> Recommender Email</h3>
+                                    <h3 className="text-2xl font-medium text-pink-600 text-center">{recommenderEmail}</h3>
+                                    <br />
+                                    <h3 className="text-2xl font-medium text-black text-center">{recommendDate}</h3>
+                                </div>
                             </div>
                         </td>
                     </tr>
                 </tbody>
-                
+
 
             </table>
         </div>
