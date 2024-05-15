58bff08ad5fec537f8b65ab1650b8cc3c178d7db
chandro4D <chandroshakhor7@gmail.com>
2024-05-15 23:13:21 +0600

    add pictura1

--- src/Components/MyRecommendCard/MyRecommendCard.jsx
+++ src/Components/MyRecommendCard/MyRecommendCard.jsx
@@ -3,7 +3,7 @@
 const MyRecommendCard = ({ recommendOne }) => {
     const { RecommendedproductName, recomenderName, RecommendedProductImage,recommenderEmail,recommendDate } = recommendOne;
     return (
-        <div className="overflow-x-auto bg-slate-300 mb-2 w-[1300px] ml-[100px]">
+        <div className="overflow-x-auto bg-slate-300 mb-2 w-[1320px] ml-[100px]">
             <table className="table">
                 <tbody>
                     <tr>
@@ -19,16 +19,19 @@ const MyRecommendCard = ({ recommendOne }) => {
                                     <br />
                                     <h3 className="text-2xl font-medium text-pink-600 text-center">{ RecommendedproductName}</h3>
                                 </div>
-                                <div className="mr-20 ">
+                                <div className="mr-12 ">
                                     <h3 className="text-xl font-medium text-sky-600"> Recomender Name</h3>
                                     <br />
                                     <h3 className="text-2xl font-medium text-pink-600 text-center">{ recomenderName}</h3>
                                 </div>
-                                <div className="text-center">
+                                <div className="text-center mr-5">
                                     <h3 className="text-xl font-medium text-sky-600"> Recommender Email</h3>
                                     <h3 className="text-2xl font-medium text-pink-600 text-center">{recommenderEmail}</h3>
                                     <br />
                                     <h3 className="text-2xl font-medium text-black text-center">{recommendDate}</h3>
+                                </div>
+                                <div className=" mt-[60px]  ">
+                                    <button className="text-center w-[60px] h-[40px] bg-red-600 rounded-[5px] ">Delete</button>
                                 </div>
                             </div>
                         </td>
