abf464f33fbe1a9651f057903ecc19e0857eca16
chandro4D <chandroshakhor7@gmail.com>
2024-05-15 21:28:45 +0600

    add recommendation for me

--- src/Components/RecomendationForMe/RecomendationForMe.jsx
+++ src/Components/RecomendationForMe/RecomendationForMe.jsx
@@ -1,11 +1,29 @@
-
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { useState } from "react";
+import { AuthContext } from "../../AuthProviders/AuthProviders";
+import MyRecommendCard from "../MyRecommendCard/MyRecommendCard";
 
 const RecomendationForMe = () => {
+    const { user } = useContext(AuthContext)
+    const [myRecommend, setMyRecommend] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(
+                `${import.meta.env.VITE_API_URL}/recommend/${user?.email}`
+            )
+            setMyRecommend(data)
+        }
+        getData()
+    }, [user])
+    console.log(myRecommend)
     return (
-        <div>
-            <h3 className="text-center text-blue text-4xl"> Recomendation
-            For Me</h3>
-        </div>
+        <div className="mb-[60px]">        
+        {
+            myRecommend.map(recommendOne => <MyRecommendCard key={recommendOne._id} recommendOne={recommendOne}></MyRecommendCard>)
+        }
+    </div>
     );
 };
 
